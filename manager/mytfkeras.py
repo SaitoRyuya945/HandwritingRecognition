fbfcbfbf26464f9172de40499ba86267c30c2980
SaitoRyuya945 <saitoryuya945@gmail.com>
2020-07-02 17:51:23 +0900

    モデルの中間層を

--- manager/mytfkeras.py
+++ manager/mytfkeras.py
@@ -14,6 +14,20 @@ import cv2
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
-
 def static_load():
     s = models.load_model("manager/static/manager/deepmodel/mnistraining.h5")
+    return s
+
+def model_predict():
+    model = static_load()
+    model.evaluate(test_images, test_labels, verbose=2)
+    w = model.layers[0].get_weights()[0]
+    fig = plt.figure(figsize=(10,10))
+    for i in range(64):
+        plt.subplot(8,8,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(w[:,:,0,i].reshape(3,3), cmap =
+                plt.cm.binary)
+    fig.savefig("manager/static/manager/deepmodel/conv2_1.png")
